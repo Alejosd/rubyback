c24f9b9109144e94da5f0c2b801c4ebe11c2b9ef
Alejosd <alejosd5@gmail.com>
2016-04-23 21:02:34 -0300

    Implementacion framework sinatra server request POST /users/save con conexion a mongodb para insert

--- app/backend/server.rb
+++ app/backend/server.rb
@@ -12,14 +12,27 @@ db= ENV['MONGO_DB'] || 'doom'
 puts "Connecting to #{host}:#{port}"
 
 client = Mongo::Client.new(["#{host}:#{port}"],:database => db,:max_pool_size => 5)
+
 set :port, 8081
 
-get '/users/:name' do
+get '/users/:username' do
 
- username="#{params['name']}"
+ username="#{params['username']}"
  collection_users = :users
  bson=client[collection_users].find(:username => username).projection(:_id => 0,:username => 1).to_a
  content_type :json
- bson.to_json
-#content
+ bson[0].to_json
+
+end
+
+
+post '/users/save' do
+
+  content= JSON.parse(request.body.read)
+  collection_users = :users
+  bson=client[collection_users].insert_one({ username: content["username"] })
+  [200, {}, {:user => "created"}.to_json]
+
+
+
 end
